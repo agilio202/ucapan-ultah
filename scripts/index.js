98d56d353157c110b9943e83548dc5454aa2606a
agilio202 <agildarma911@gmail.com>
2025-07-09 13:23:15 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,11 +10,11 @@ function validateForm() {
   var password = document.getElementById("password").value;
 
   //validasi username dan password, username dan password bisa diganti sesuai keinginan
-  if (username === "user" && password === "user") {
+  if (username === "anngun cantik" && password === "anggun cantik") {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
-      text: "Selamat datang ayanggg",
+      text: "Selamat datang anggun cantik",
       showConfirmButton: false,
       timer: 1500,
     }).then(function () {
